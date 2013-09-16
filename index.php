8cacabe3575ceae4c771ff801210e62b07424d0e
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-09-15 22:36:01 -0500

    Added comments for separating constant categories, added FILE_WORLD constant and replaced previous string uses with it, added conditional leaflet.ie.css reference, and changed map code.

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+// Basic
 define("NAME", "CleanApp");
+
+// Versions
 define("VER", "2.0");
 define("VER_JQ", "1.10.2");
 define("VER_JQUI", "1.10.3");
+
+// Directories
 define("DIR_ASSETS", "assets");
 define("DIR_CSS", DIR_ASSETS."/css");
 define("DIR_DATA", DIR_ASSETS."/data");
@@ -12,7 +17,21 @@ define("DIR_CON", DIR_JS."/Console");
 define("DIR_JQUI", DIR_JS."/jquery-ui");
 define("DIR_LEAF", DIR_JS."/leaflet");
 define("DIR_LEAF_PLUG", DIR_JS."/leaflet/plugins");
-?><!doctype html>
+
+// Files
+define("FILE_WORLD", DIR_DATA."/world.geojson");
+
+// Get a list of IDs from the world file.
+$world = json_decode(file_get_contents(FILE_WORLD));
+$ids = array();
+foreach ($world as $collection) {
+	$features = $collection->features;
+	foreach ($features as $feature) {
+		$id = $feature->id;
+		array_push($ids, $id);
+	}
+}
+?><!-- <?php //print_r($ids, true)?> --><!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -32,9 +51,10 @@ define("DIR_LEAF_PLUG", DIR_JS."/leaflet/plugins");
 <script src="<?=DIR_JS?>/shorty/jquery.shorty.min.js" type="text/javascript"></script>
 <!-- Leaflet -->
 <link rel="stylesheet" href="<?=DIR_LEAF?>/leaflet.css" type="text/css">
+<!--[if lte IE 8]><link rel="stylesheet" href="<?=DIR_LEAF?>/leaflet.ie.css" /><![endif]-->
 <script src="<?=DIR_LEAF?>/leaflet.js" type="text/javascript"></script>
 <!-- Map assets -->
-<script src="<?=DIR_DATA?>/world.js" type="text/javascript"></script>
+<script src="<?=FILE_WORLD?>" type="text/javascript"></script>
 <script src="<?=DIR_LEAF_PLUG?>/Leaflet.TileLayer.Common.js" type="text/javascript"></script>
 <!-- Proprietary -->
 <link rel="stylesheet" href="<?=DIR_CSS?>/styles.css" type="text/css">
@@ -42,17 +62,104 @@ define("DIR_LEAF_PLUG", DIR_JS."/leaflet/plugins");
 <script type="text/javascript">
 (function($) {
 	$(function() {
-		/*var map2 = L.map('map2').setView([37.8, -96], 4);
-		L.tileLayer(CA.globals.const.CM_URL, {attribution: CM_ATTR, styleId: 22677}).addTo(map);
-		L.geoJson(statesData).addTo(map);
-		var map3 = L.map('map3').setView([37.8, -96], 4);
-		L.tileLayer(CA.globals.const.CM_URL, {attribution: CM_ATTR, styleId: 22677}).addTo(map3);
-		L.geoJson(statesData, {style: style}).addTo(map3);*/
 		var map = L.map('map', {
-			attributionControl: false
-		}).setView([35, -89], 13);
-		//L.TileLayer.OpenStreetMap().addTo(map);
-		L.tileLayer(CA.globals.const.URL_CM, {styleId: 997}).addTo(map);
+				attributionControl: false
+			}),
+			featureData = {},
+			worldData = "";
+	
+		L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery Â© <a href="http://cloudmade.com">CloudMade</a>'
+		}).addTo(map);
+	
+		function onLocationFound(e) {
+			var radius = e.accuracy / 2;
+			if (radius.hasDecimal())
+				radius = radius.toFixed(1);
+			L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point");//.openPopup();
+			L.circle(e.latlng, radius).addTo(map);
+			Console.debug("Radius: ", radius);
+		}
+	
+		function onLocationError(e) {
+			alert(e.message);
+		}
+		
+		// get color depending on population density value
+		function getColor(id) {
+			var color;
+			/*$.ajax({
+				async: false,
+				type: "GET",
+				url: "check.php",
+				data: {
+					id: id
+				},
+				success: function(data) {
+					if (data == "true")
+						color = "#00FF00";
+					else
+						color = "#FF0000";
+				}
+			});*/
+			return color;
+		}
+	
+		function style(feature) {
+			return {
+				weight: 2,
+				opacity: 1,
+				color: 'white',
+				dashArray: '3',
+				fillOpacity: 0.7,
+				fillColor: getColor(feature.id)
+			};
+		}
+	
+		function highlightFeature(e) {
+			var layer = e.target;
+	
+			layer.setStyle({
+				weight: 5,
+				color: '#666',
+				dashArray: '',
+				fillOpacity: 0.7
+			});
+	
+			if (!L.Browser.ie && !L.Browser.opera) {
+				layer.bringToFront();
+			}
+	
+			//info.update(layer.feature.properties);
+		}
+	
+		function resetHighlight(e) {
+			geojson.resetStyle(e.target);
+			//info.update();
+		}
+	
+		function zoomToFeature(e) {
+			map.fitBounds(e.target.getBounds());
+		}
+	
+		function onEachFeature(feature, layer) {
+			layer.on({
+				mouseover: highlightFeature,
+				mouseout: resetHighlight,
+				click: zoomToFeature
+			});
+		}
+	
+		geojson = L.geoJson(worldData, {
+			style: style,
+			onEachFeature: onEachFeature
+		}).addTo(map);
+		
+		map.on('locationfound', onLocationFound);
+		map.on('locationerror', onLocationError);
+	
+		map.locate({setView: true, maxZoom: 16});
 	});
 })(jQuery);
 </script>
