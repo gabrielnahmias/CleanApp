5aff9862735117f7abaa3eb2e3cecbe42b9afcf7
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-09-17 17:16:59 -0500

    Reverted (kinda).

--- get_data.php
+++ get_data.php
@@ -1,15 +1,17 @@
 <?php
-require "common.php";
+require_once "common.php";
 
 header("Content-type: text/javascript");
 
 if (isset($_GET['f']))
 	$file = $_GET['f'];
 else
-	die("No file specified (query parameter `f`).");
+	$file = FILE_WORLD;
+	//die("No file specified (query parameter `f`).");
 	
 if (isset($_GET['v']))
 	$var = $_GET['v'];
 else
-	die("No variable specified (query parameter `v`).");
+	$var = "var worldData";
+	//die("No variable specified (query parameter `v`).");
 ?><?=$var?> = <?php echo file_get_contents($file); ?>;
