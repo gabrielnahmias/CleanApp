e731607cc055244c55acac92f2d1e86ed6186ffa
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-09-17 17:14:30 -0500

    Added CONFIG_DEBUG, NAME_RESPONDER, DIR_CLS, and DIR_INC.

--- config.php
+++ config.php
@@ -1,6 +1,14 @@
 <?php
+
+// Configuration settings
+
+define("CONFIG_DEBUG", true);
+
+// Constants
+
 // Basic
 define("NAME", "CleanApp");
+define("NAME_RESPONDER", "Add Responder Data");
 define("SEP", " - ");
 define("MOTTO", "Restoring the Planet");
 define("TITLE", NAME.SEP.MOTTO);
@@ -13,8 +21,10 @@ define("VER_JQUI", "1.10.3");
 // Directories
 define("DIR_ASSETS", "assets");
 define("DIR_CSS", DIR_ASSETS."/css");
+define("DIR_CLS", DIR_ASSETS."/cls");
 define("DIR_DATA", DIR_ASSETS."/data");
 define("DIR_IMG", DIR_ASSETS."/img");
+define("DIR_INC", DIR_ASSETS."/inc");
 define("DIR_JS", DIR_ASSETS."/js");
 define("DIR_CON", DIR_JS."/Console");
 define("DIR_JQUI", DIR_JS."/jquery-ui");
