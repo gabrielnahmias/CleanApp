e5c2b199e8d6e5a5c681a5136e36abf92e0bd9fb
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-09-17 17:12:47 -0500

    Included class files and added Browser instance with related method calls.

--- common.php
+++ common.php
@@ -1,3 +1,14 @@
 <?php
 
-require "config.php";
+require_once "config.php";
+require_once DIR_CLS."/Browser/Browser.php";
+require_once DIR_CLS."/Web.php";
+
+$br = new Browser();
+
+// Normalize some commonly referred-to values.
+$browser = $br->getBrowserArray();
+// Possible future overrides:
+//$browser['classString'] = $br->getClassString("", true, true, true, true);
+
+//$db = mysql_connect("localhost", "gabriel", "shitfuck1");
