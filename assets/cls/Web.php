36f0c40d2bbb77496fab8e7474c80990d374a333
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-09-20 22:14:20 -0500

    Removed else clause from CONFIG_DEBUG-based conditional.

--- assets/cls/Web.php
+++ assets/cls/Web.php
@@ -5,9 +5,7 @@ require_once "config.php";
 class Web {
 	public static function debug($val, $title = "Data") {
 		if (CONFIG_DEBUG):
-			echo "<!--\r\n$title:\r\n".print_r($val, true)."-->";
-		else:
-			echo "SHIT!!";
+			echo "<!--\r\n$title:\r\n".print_r($val, true)."\r\n-->";
 		endif;
 	}
 	public function filter($var, $option = FILTER_SANITIZE_STRING) {
