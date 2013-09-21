698d9359e36364dcd8af15c457516e7d185ec9f4
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-09-20 22:41:15 -0500

    Changed initial Web::debug() call, added jQuery Center Plugin for experimentation, added individual comments for map assets, added leaflet.overintent plugin, added commented references to the main Leaflet.Storage plugin, made map variable global again, added empty mouseintent event to each feature (highlightable geographical area), added extra comments to the iPhone address bar hiding code and modified it to not apply to standalone instances (native web application mode), added commented dblclick (double click) map event, added content section surrounding the map element, and removed useless script block.

--- index.php
+++ index.php
@@ -19,13 +19,13 @@ foreach ($collection->features as $feature) {
 				);
 }
 
-Web::debug($browser, "Browser Data");
+Web::debug($br->isMobile() ? "Yes" : "No", "Is this browser mobile");
 ?><!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
 <?php include DIR_INC."/apple_meta.inc.php" ?>
-<title><?=TITLE?></title>
+<title><?=($browser['iOS']) ? NAME :TITLE?></title>
 <link rel="shortcut icon" href="<?=DIR_IMG?>/favicon.ico">
 <!-- normalize.css -->
 <link rel="stylesheet" href="<?=DIR_CSS?>/normalize.min.css" type="text/css">
@@ -33,6 +33,8 @@ Web::debug($browser, "Browser Data");
 <script src="<?=DIR_CON?>/Console.js" type="text/javascript"></script>
 <!-- jQuery -->
 <script src="<?=DIR_JS?>/jquery-<?=VER_JQ?>.min.js" type="text/javascript"></script>
+<!-- jQuery Center Plugin 
+<script src="<?php echo DIR_JS; ?>/jquery.center.min.js" type="text/javascript"></script> -->
 <!-- jQuery UI -->
 <link href="<?=DIR_JQUI?>/css/redmond/jquery-ui-<?=VER_JQUI?>.min.css" rel="stylesheet" type="text/css">
 <script src="<?=DIR_JQUI?>/jquery-ui-<?=VER_JQUI?>.min.js" type="text/javascript"></script>
@@ -43,14 +45,42 @@ Web::debug($browser, "Browser Data");
 <!--[if lte IE 8]><link rel="stylesheet" href="<?=DIR_LEAF?>/leaflet.ie.css" /><![endif]-->
 <script src="<?=DIR_LEAF?>/leaflet.js" type="text/javascript"></script>
 <!-- Map assets -->
+<!-- Get world data (GeoJSON object) -->
 <script src="<?=FILE_GETDATA?>" type="text/javascript"></script>
+<!-- Plugins -->
 <script src="<?=DIR_LEAF_PLUG?>/Leaflet.TileLayer.Common.js" type="text/javascript"></script>
+<script src="<?=DIR_LEAF_PLUG?>/leaflet.overintent.js" type="text/javascript"></script>
+<!-- Storage plugin -->
+<!--<link rel="stylesheet" href="<?=DIR_LEAF_PLUG?>/store/reqs/draw/dist/leaflet.draw.css" />
+<link rel="stylesheet" href="<?=DIR_LEAF_PLUG?>/store/reqs/minimap/src/Control.MiniMap.css" />
+<link rel="stylesheet" href="<?=DIR_LEAF_PLUG?>/store/reqs/edit_in_osm/Leaflet.EditInOSM.css" />
+<link rel="stylesheet" href="<?=DIR_LEAF_PLUG?>/store/src/css/storage.css" />
+<link rel="stylesheet" href="<?=DIR_LEAF_PLUG?>/store/contrib/css/storage.ui.default.css" />-->
+<!--<script src="<?=DIR_LEAF_PLUG?>/store/reqs/draw/dist/leaflet.draw-src.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/reqs/hash/leaflet-hash.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/reqs/i18n/Leaflet.i18n.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/reqs/edit_in_osm/Leaflet.EditInOSM.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/reqs/minimap/src/Control.MiniMap.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/src/js/leaflet.storage.core.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/src/js/leaflet.storage.xhr.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/src/js/leaflet.storage.forms.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/src/js/leaflet.lazygeojson.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/src/js/leaflet.storage.icon.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/src/js/leaflet.storage.features.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/src/js/leaflet.storage.layer.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/src/js/leaflet.storage.controls.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/src/js/leaflet.storage.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/contrib/js/storage.ui.default.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/test/_pre.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/test/Map.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/test/DataLayer.js"></script>
+<script src="<?=DIR_LEAF_PLUG?>/store/test/Poly.js"></script>-->
 <!-- Proprietary -->
 <link rel="stylesheet" href="<?=DIR_CSS?>/styles.css" type="text/css">
 <script src="<?=DIR_JS?>/scripts.js" type="text/javascript"></script>
 <script type="text/javascript">
 // Set to global for debugging purposes.
-//var map;
+var map;
 <?php if (!CONFIG_DEBUG): ?>
 Console.setOption("enabled", false);
 <?php endif; ?>
@@ -64,13 +94,13 @@ Console.setOption("enabled", false);
 				webkit: /webkit/gi.test(ua),
 			},
 			featureData = <?=json_encode($countryInfo)?>,
-			map = L.map('map', {
-				attributionControl: false
-			}),
 			popup = L.popup({
 				keepInView: true
 			});
-		
+		map = L.map('map', {
+			attributionControl: false,
+			worldCopyJump: false
+		});
 		Console.debug(featureData);
 		
 		L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
@@ -148,6 +178,9 @@ Console.setOption("enabled", false);
 	
 		function onEachFeature(feature, layer) {
 			layer.on({
+				mouseintent: function(e){
+					// Add name to layer.
+				},
 				mouseover: highlightFeature,
 				mouseout: resetHighlight,
 				click: showForm
@@ -159,9 +192,14 @@ Console.setOption("enabled", false);
 			onEachFeature: onEachFeature
 		}).addTo(map);
 		
+		// Hide Mobile Safari Address Bar
+		
 		// Instead of this, put a margin-bottom on the page and that should accomplish
 		// everything. Make it subclassed by the iPhone or and iPad page classes.
-		/*if (currentBrowser.ios) {
+		// ^^^^ NOT WORKING ^^^^
+		// This is ok for now but it makes the page vertically off-center a bit. NEED
+		// A NEW SOLUTION.
+		if (currentBrowser.ios) {
 			function orientationChange(e) {
 				/*if(e.orientation){
 					if(e.orientation == 'portrait'){
@@ -170,13 +208,21 @@ Console.setOption("enabled", false);
 					else if(e.orientation == 'landscape') {
 						//
 					}
-				}*//*
+				}*/
 				window.scrollTo(0, 1);
 			}
-			$("body").css({ height: "+=300" }).scrollTop(1);
-			$(window).bind("orientationchange", orientationChange);
-		}*/
+			if ("standalone" in window.navigator && !window.navigator.standalone) {
+				$("body").css({ height: "+=300" });
+				$(window).bind("orientationchange", orientationChange)
+						 .trigger("orientationchange");
+			}
+			//map.setView(new L.LatLng(map.getCenter()['lat'], map.getCenter()['lng']));
+		}
 		
+		/*map.on('dblclick', function(e){
+			alert('dblclick');
+			e.preventDefault();
+		});*/
 		map.on('locationfound', onLocationFound);
 		map.on('locationerror', onLocationError);
 		
@@ -187,12 +233,9 @@ Console.setOption("enabled", false);
 </head>
 <body<?=$browser['classString']?>>
 	<div id="wrapper">
-    	<div id="map"></div>
+    	<section id="content">
+    		<div id="map"></div>
+        </section>
     </div>
-    <script type="text/javascript">
-	(function () {
-		
-	})();
-	</script>
 </body>
 </html>
